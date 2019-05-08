b1c2a5bea9a19ffd1c004c15f712730313516efc
ayu <2050030064@qq.com>
2019-05-08 10:34:57 +0800

    yu diqu

---
+++ DIquzhaoshangboss/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps")
+
+gulp.task('sass',function(){
+	gulp.src("*.scss")
+	.pipe(sourcemaps.init())
+	.pipe(sass())
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest("css"))
+})
+
+
+gulp.task("watch",function(){
+	gulp.watch("*.scss",["sass"])
+})
